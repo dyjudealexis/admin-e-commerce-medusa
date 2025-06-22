3edc6542c06fde86cfa41feb18c52836e7017305
Jude Alexis Dy <dyjudealexis@gmail.com>
2025-06-22 17:15:53 +0800

    update

---
+++ .medusa/server/public/admin/assets/chunk-FJEVKJ5V-DIySLCkv.js
@@ -0,0 +1,1 @@
+import{al as i}from"./index-D74VZicr.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
---
+++ .medusa/server/public/admin/assets/chunk-GW6TVOAA-DygYEXrw.js
@@ -0,0 +1,1 @@
+import{u as e}from"./chunk-W7625H47-CNk704JE.js";var a=()=>e(),s=()=>e();export{s as a,a as u};
